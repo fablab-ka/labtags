c09b05d948ced5cad1ea1df2eb5db6e1921c0629
Sven Hecht <sven.hecht@citrix.com>
2017-02-11 00:34:20 +0100

    improved invalid message handling

--- blot-gateway/client.py
+++ blot-gateway/client.py
@@ -22,11 +22,15 @@ class Client:
 
         for commandMessage in result:
             if commandMessage and commandMessage.has_key("command"):
+                if not isinstance(commandMessage["command"], string):
+                    print("invalid message format")
+                    continue
+
                 if commandMessage["command"] == COMMAND_CONNECT_TAG:
-                    print("[Client] Received Connect Command for Tag '" + commandMessage.mac + "'")
+                    print("[Client] Received Connect Command for Tag '" + commandMessage.tag_mac + "'")
 
                     self.queueLock.acquire()
-                    self.messageQueue.put(ConnectToTagCommandMessage(commandMessage.mac))
+                    self.messageQueue.put(ConnectToTagCommandMessage(commandMessage.tag_mac))
                     self.queueLock.release()
                 else:
                     print("[Client] unknown result command '" + commandMessage["command"] + "'")
