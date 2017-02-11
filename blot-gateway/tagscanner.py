0de87a65bc35b65151473ccc19fce74854cfe3b9
Sven Hecht <sven.hecht@citrix.com>
2017-02-11 01:04:45 +0100

    added log line

--- blot-gateway/tagscanner.py
+++ blot-gateway/tagscanner.py
@@ -21,7 +21,7 @@ class TagScanner:
                 print("[TagScanner] Device not connectable", d.addr)
                 continue
 
-            #print("[TagScanner] DEBUG", dir(d))
+            print("[TagScanner] Tag found " + str(d))
             result.append(Tag(d.addr, d.addrType))
 
         return result
