b2fdd326a8bdc1ee23b239a80a0da1248f5b151a
Sven <sven.hecht@gmail.com>
2018-06-27 23:20:06 +0200

    Remove superfluous response handling

--- blot-gateway/mqttClient.py
+++ blot-gateway/mqttClient.py
@@ -79,12 +79,3 @@ class MQTTClient:
                                                                         )))
             except BaseException as e:
                 print((ANSI_YELLOW + "Error relaying message {%s} '%s'. Error: {%s}" + ANSI_OFF) % (payload, topic, e))
-
-            if res.text == "" or res.text == "OK":
-                print(ANSI_YELLOW + "[MQTTClient] No Command response" + ANSI_OFF)
-            else:
-                try:
-                    self.handleResponse(res.json())
-                except:
-                    print(
-                        ANSI_YELLOW + "[MQTTClient] Broken Server response '" + str(res.text) + "'" + ANSI_OFF)
